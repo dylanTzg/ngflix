0b0422bf7be8251a632618725f980e586f020349
dylantizguine <dylantizguine@gmail.com>
2024-02-25 19:39:08 +0100

    add new components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,19 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import {FooterComponent} from "./shared/footer/footer.component";
 import { HeaderComponent } from './shared/header/header.component';
+import { HomeComponent } from './pages/home/home.component';
+import { MovieListComponent } from './pages/movie-list/movie-list.component';
+import { SliderComponent } from './components/slider/slider.component';
+
 
 @NgModule({
   declarations: [
     AppComponent,
     FooterComponent,
-    HeaderComponent
+    HeaderComponent,
+    HomeComponent,
+    MovieListComponent,
+    SliderComponent,
   ],
   imports: [
     BrowserModule,
